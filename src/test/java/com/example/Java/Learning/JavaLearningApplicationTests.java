1ca52fee31f66760908ca27883696411241a08cb
madhava <madhava@divum.in>
2024-01-10 12:45:08 +0530

    initial code setup

---
+++ src/test/java/com/example/Java/Learning/JavaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java.Learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
